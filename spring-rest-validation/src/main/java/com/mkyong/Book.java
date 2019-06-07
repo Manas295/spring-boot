8cf187eca6670e12896d7a02f9b2ef97dca98040
Manas295 <manasbansal672@gmail.com>
2019-06-07 22:16:20 +0530

    Spring Boot Request Validation

---
+++ spring-rest-validation/src/main/java/com/mkyong/Book.java
@@ -0,0 +1,60 @@
+package com.mkyong;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+import com.mkyong.error.validator.Author;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@Entity
+public class Book {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @NotEmpty(message = "Please provide a name")
+    private String name;
+
+    @Author
+    @NotEmpty(message = "Please provide a author")
+    private String author;
+
+    @NotNull(message = "Please provide a price")
+    @DecimalMin("1.00")
+    private BigDecimal price;
+
+    // avoid this "No default constructor for entity"
+    public Book() {
+    }
+
+    public Book(Long id, String name, String author, BigDecimal price) {
+        this.id = id;
+        this.name = name;
+        this.author = author;
+        this.price = price;
+    }
+
+    public Book(String name, String author, BigDecimal price) {
+        this.name = name;
+        this.author = author;
+        this.price = price;
+    }
+
+	/*
+	 * @Override public String toString() { return "Book{" + "id=" + id + ", name='"
+	 * + name + '\'' + ", author='" + author + '\'' + ", price=" + price + '}'; }
+	 */
+}
